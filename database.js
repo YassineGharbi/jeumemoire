7e5eeb65e15e8af357a057505c0fbf274eb7757c
Yassine Gharbi <y.gharbi@gmail.com>
2020-11-26 13:19:57 +0100

    Infos BDD

--- database.js
+++ database.js
@@ -2,25 +2,25 @@
 var mysql = require('mysql');
 
 //2- CONNECTION A LA BASE DE DONNEES EN LOCAL
-const dbConnexion = mysql.createConnection({
-    host: "localhost",
-    database: "jeu_memoire",
-    user: "root",
-    password: "root",
-    port: 8889,
-    // socketPath est disponible sur la page d'acceuil du MAMP Local
-    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
-    strict: false //Permets d'entrer des valeurs NULL pour les champs date
-});
+// const dbConnexion = mysql.createConnection({
+//     host: "localhost",
+//     database: "jeu_memoire",
+//     user: "root",
+//     password: "root",
+//     port: 8889,
+//     // socketPath est disponible sur la page d'acceuil du MAMP Local
+//     socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock',
+//     strict: false //Permets d'entrer des valeurs NULL pour les champs date
+// });
 
 // VERSION EN LIGNE HEROKU UNIQUEMENT AVEC createPool QUI STOCK DES CONNEXIONS
-// const dbConnexion = mysql.createPool({
-//     host: "eu-cdbr-west-03.cleardb.net",
-//     database: "heroku_71544a75e5b01d8",
-//     user: "b2332a087876af",
-//     // port: 5432,
-//     password: "b7e2b2b3",
-// });
+const dbConnexion = mysql.createPool({
+    host: "eu-cdbr-west-03.cleardb.net",
+    database: "heroku_63ee4254d14e96b",
+    user: "b5d1857fc55a44",
+    // port: 5432,
+    password: "cc113c6c",
+});
 
 // Test de la connexion à la base
 dbConnexion.query('select 1 + 1', (err, rows) => {
